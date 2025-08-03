af9ba58b4947dc5f3d8e106b032ad440c8643d00
Agustin <jagustin.rm@gmail.com>
2025-08-03 12:39:58 -0300

    addShips

--- Port.cs
+++ Port.cs
@@ -8,37 +8,71 @@ using System.Threading.Tasks;
 namespace Models
 {
     public class Port
-{
-    public string Name { get; set; }
-    public int PortNumber { get; set; }
-    public List<Ship> Ships { get; set; }
+    {
+        public string Name { get; set; }
+        public int PortNumber { get; set; }
+        public List<Ship> Ships { get; set; }
 
         public Port(string name)
         {
-        Name = name;
-        Ships = new List<Ship>();
+            Name = name;
+            Ships = new List<Ship>();
         }
-    public void PrintInfo()
-    {
-        Console.WriteLine($"Port name: {Name}");
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Port name: {Name}");
+            Console.WriteLine($"Port number: {PortNumber}");
+            Console.WriteLine("Ships in the port: " + Ships.Count);
+            Console.WriteLine("Total load capacity: " + GetTotalLoadCapacity() + " tons");
+
+        }
+
+        public void AddShip(Ship ship)
+        {
+            Ships.Add(ship);
+        }
+        public int GetTotalLoadCapacity()
+        {
+            return Ships.Sum(s => s.GetLoadCapacity());
+        }
     }
-}
 
-public class Ship
-{
-    public string Name { get; set; }
+    public abstract class Ship
+    {
+        public string Name { get; set; }
 
-    public int YearBuilt { get; set; }
+        public int YearBuilt { get; set; }
 
-    public string Brand { get; set; }
+        public string Brand { get; set; }
 
-    public string Type { get; set; }
+        public string Type { get; set; }
 
-    public int Capacity { get; set; }
+        // public int Capacity { get; set; }
+        public abstract int GetLoadCapacity();
+
+        // public List<Port> Ports { get; set; }
+    }
+
+
+    public class CargoShip : Ship
+    {
+        public int MaxTons { get; set; }
+
+        public override int GetLoadCapacity()
+        {
+            return MaxTons;
+        }
+    }
     
-    // public List<Port> Ports { get; set; }
-}
+    public class PassengerShip : Ship
+    {
+        public int MaxPassengers { get; set; }
 
+        public override int GetLoadCapacity()
+        {
+            return MaxPassengers * 100;
+        }
+    }
     
 }
 
--- Program.cs
+++ Program.cs
@@ -9,9 +9,97 @@ class Program
     static void Main()
     {
         // Acá empieza el programa
-        Console.WriteLine("Hola mundo!");
-        Port mc = new Port("Maracaibo");
+        Console.WriteLine("Name of the port:");
+        string portName = "Puerto1";
+        int PortNumber = 1234;
+
+        Port mc = new Port(portName);
+        mc.PortNumber = PortNumber;
+        mc.AddShip(new CargoShip { Name = "Cargo1", YearBuilt = 2000, Brand = "BrandA", Type = "Cargo", MaxTons = 500 });
+        mc.AddShip(new CargoShip { Name = "Cargo2", YearBuilt = 2005, Brand = "BrandB", Type = "Cargo", MaxTons = 1000 });
         mc.PrintInfo();
+        string res = "0";
+        Console.WriteLine("Welcome to the port management system!");
+        while (res != "3")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Print port information");
+            Console.WriteLine("2. Add a ship to the port");
+            Console.WriteLine("4. List all ships in the port");
+            Console.WriteLine("5. Find a ship by name");
+            Console.WriteLine("3. Exit");
+            res = Console.ReadLine().ToString();
+
+            if (res == "1")
+            {
+                mc.PrintInfo();
+            }
+            else if (res == "2")
+            {
+                Console.WriteLine("Type of ship to add:");
+                Console.WriteLine("1. Cargo Ship");
+                Console.WriteLine("2. Passenger Ship");
+                string shipType = Console.ReadLine();
+                Console.WriteLine("Enter ship name:");
+                string shipName = Console.ReadLine();
+                Console.WriteLine("Enter year built:");
+                int yearBuilt = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter brand:");
+                string brand = Console.ReadLine();
+                // Console.WriteLine("Enter capacity:");
+                // int capacity = int.Parse(Console.ReadLine());
+                if (shipType == "1")
+                {
+                    Console.WriteLine("Enter max tons:");
+                    int maxTons = int.Parse(Console.ReadLine());
+
+                    CargoShip newShip = new CargoShip
+                    {
+                        Name = shipName,
+                        YearBuilt = yearBuilt,
+                        Brand = brand,
+                        Type = "Cargo",
+                        MaxTons = maxTons
+                    };
+
+                    mc.AddShip(newShip);
+                    Console.WriteLine($"Cargo ship {shipName} added to the port.");
+                }
+                else if (shipType == "2")
+                {
+                    Console.WriteLine("Enter max passengers:");
+                    int MaxPassengers = int.Parse(Console.ReadLine());
+                    PassengerShip passengerShip = new PassengerShip
+
+                    {
+                        Name = shipName,
+                        YearBuilt = yearBuilt,
+                        Brand = brand,
+                        Type = "Passenger",
+                        MaxPassengers = MaxPassengers
+                    };
+                    mc.AddShip(passengerShip);
+                    Console.WriteLine($"Passenger ship {shipName} added to the port."); 
+                }
+                else
+                {
+                    Console.WriteLine("Invalid ship type selected.");
+                }
+            }
+            else if (res == "3")
+            {
+                Console.WriteLine("Exiting the program. Goodbye!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option selected.");
+            }
+            Console.WriteLine("Press Enter to continue...");
+            res = Console.ReadLine().ToString();
+        }
+
     }
 }
 
