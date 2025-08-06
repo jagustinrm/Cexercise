c50c7a0bbbfb50af1ce80680cb8b12dd76ede9e1
Agustín <jagustin.rm@gmail.com>
2025-08-05 23:41:32 -0300

    step 1 completed

--- Port.cs
+++ Port.cs
@@ -3,42 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models.Ships;
 
 
-namespace Models
+namespace Models.Ports
 {
     public class Port
-{
-    public string Name { get; set; }
-    public int PortNumber { get; set; }
-    public List<Ship> Ships { get; set; }
+    {
+        public string Name { get; set; }
+        public int PortNumber { get; set; }
+        public List<Ship> Ships { get; set; }
+
 
         public Port(string name)
         {
-        Name = name;
-        Ships = new List<Ship>();
+            Name = name;
+            Ships = new List<Ship>();
         }
-    public void PrintInfo()
-    {
-        Console.WriteLine($"Port name: {Name}");
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Port name: {Name}");
+            Console.WriteLine($"Port number: {PortNumber}");
+            Console.WriteLine("Ships in the port: " + Ships.Count);
+            Console.WriteLine("Total load capacity: " + GetTotalLoadCapacity() + " tons");
+
+        }
+
+        public void AddShip(Ship ship)
+        {
+            Ships.Add(ship);
+        }
+        public int GetTotalLoadCapacity()
+        {
+            return Ships.Sum(s => s.GetLoadCapacity());
+        }
+        public void OrderByNameAscending()
+        {
+            var sortedShips = Ships.OrderBy(s => s.Name).ToList();
+            for (int i = 0; i < sortedShips.Count; i++)
+            {
+                Console.WriteLine(sortedShips[i].Name);
+            }
+        }
+
+        public void OrderByNameDescending()
+        {
+            var sortedShips = Ships.OrderByDescending(s => s.Name).ToList();
+            for (int i = 0; i < sortedShips.Count; i++)
+            {
+                Console.WriteLine(sortedShips[i].Name);
+            }
+        }
+
+        public void FindShipByName(string name) {
+            var ship = Ships.FirstOrDefault(s => s.Name == name);
+            if (ship != null)
+            {
+                Console.WriteLine("Ship found:");
+                Console.WriteLine($"Name: {ship.Name}");
+                Console.WriteLine($"Year built: {ship.YearBuilt}");
+                Console.WriteLine($"Brand: {ship.Brand}");
+                Console.WriteLine($"Type: {ship.Type}");
+                Console.WriteLine($"Load capacity: {ship.GetLoadCapacity()} tons");
+            }
+            else
+            {
+                Console.WriteLine("Ship not found.");
+            }
+        }
     }
 }
 
-public class Ship
-{
-    public string Name { get; set; }
+namespace Models.Ships {
+    public interface ILoadable
+    {
+        int GetLoadCapacity();
+    }
+    public abstract class Ship : ILoadable
+    {
+        public string Name { get; set; }
 
-    public int YearBuilt { get; set; }
+        public int YearBuilt { get; set; }
 
-    public string Brand { get; set; }
+        public string Brand { get; set; }
 
-    public string Type { get; set; }
+        public string Type { get; set; }
 
-    public int Capacity { get; set; }
+        public abstract int GetLoadCapacity();
+
+    }
+
+
+    public class CargoShip : Ship
+    {
+        public int MaxTons { get; set; }
+
+        public override int GetLoadCapacity()
+        {
+            return MaxTons;
+        }
+    }
     
-    // public List<Port> Ports { get; set; }
-}
+    public class PassengerShip : Ship
+    {
+        public int MaxPassengers { get; set; }
 
-    
+        public override int GetLoadCapacity()
+        {
+            return MaxPassengers * 100;
+        }
+    }
+
 }
 
--- Program.cs
+++ Program.cs
@@ -3,15 +3,126 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Models;
+using Models.Ports;
+using Models.Ships;
 class Program
 {
     static void Main()
     {
         // Acá empieza el programa
-        Console.WriteLine("Hola mundo!");
-        Port mc = new Port("Maracaibo");
+        Console.WriteLine("Name of the port:");
+        string portName = "Puerto1";
+        int PortNumber = 1234;
+
+        Port mc = new Port(portName);
+        mc.PortNumber = PortNumber;
+        mc.AddShip(new CargoShip { Name = "Cargo1", YearBuilt = 2000, Brand = "BrandA", Type = "Cargo", MaxTons = 500 });
+        mc.AddShip(new CargoShip { Name = "Cargo2", YearBuilt = 2005, Brand = "BrandB", Type = "Cargo", MaxTons = 1000 });
         mc.PrintInfo();
+        string res = "0";
+        Console.WriteLine("Welcome to the port management system!");
+        while (res != "5")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Print port information");
+            Console.WriteLine("2. Add a ship to the port");
+            Console.WriteLine("3. List all ships in the port");
+            Console.WriteLine("4. Find a ship by name");
+            Console.WriteLine("5. Exit");
+            res = Console.ReadLine().ToString();
+
+            if (res == "1")
+            {
+                mc.PrintInfo();
+            }
+            else if (res == "2")
+            {
+                Console.WriteLine("Type of ship to add:");
+                Console.WriteLine("1. Cargo Ship");
+                Console.WriteLine("2. Passenger Ship");
+                string shipType = Console.ReadLine();
+                Console.WriteLine("Enter ship name:");
+                string shipName = Console.ReadLine();
+                Console.WriteLine("Enter year built:");
+                int yearBuilt = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter brand:");
+                string brand = Console.ReadLine();
+                // Console.WriteLine("Enter capacity:");
+                // int capacity = int.Parse(Console.ReadLine());
+                if (shipType == "1")
+                {
+                    Console.WriteLine("Enter max tons:");
+                    int maxTons = int.Parse(Console.ReadLine());
+
+                    CargoShip newShip = new CargoShip
+                    {
+                        Name = shipName,
+                        YearBuilt = yearBuilt,
+                        Brand = brand,
+                        Type = "Cargo",
+                        MaxTons = maxTons
+                    };
+
+                    mc.AddShip(newShip);
+                    Console.WriteLine($"Cargo ship {shipName} added to the port.");
+                }
+                else if (shipType == "2")
+                {
+                    Console.WriteLine("Enter max passengers:");
+                    int MaxPassengers = int.Parse(Console.ReadLine());
+                    PassengerShip passengerShip = new PassengerShip
+
+                    {
+                        Name = shipName,
+                        YearBuilt = yearBuilt,
+                        Brand = brand,
+                        Type = "Passenger",
+                        MaxPassengers = MaxPassengers
+                    };
+                    mc.AddShip(passengerShip);
+                    Console.WriteLine($"Passenger ship {shipName} added to the port."); 
+                }
+                else
+                {
+                    Console.WriteLine("Invalid ship type selected.");
+                }
+            }
+            
+            else if (res == "3") 
+            {
+                Console.WriteLine("Ordenar por: 1. Nombre (Asc) 2. Nombre (Desc)");
+                string order = Console.ReadLine();
+                if (order == "1")
+                {
+                    mc.OrderByNameAscending();
+                }
+                else if (order == "2")
+                {
+                    mc.OrderByNameDescending();
+                }
+
+                
+            }
+            else if (res == "4") 
+            {
+                Console.WriteLine("Enter the name of the ship to find:");
+                string shipName = Console.ReadLine();
+                mc.FindShipByName(shipName);
+            }
+            else if (res == "5")
+            {
+                Console.WriteLine("Exiting the program. Goodbye!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option selected.");
+            }
+            Console.WriteLine("Press Enter to continue...");
+            res = Console.ReadLine().ToString();
+        }
+
     }
 }
 
